3261056978033b6613c55abf6596c79ac74963ee
ALFAJRI_BOT-WA <82745960+AlFajri441@users.noreply.github.com>
2021-04-19 12:19:09 +0700

    Add files via upload

---
+++ donasi.js
@@ -0,0 +1,21 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃       𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DINAI PAGGES ❉⊰━━✿
+┃  
+┣━⊱ *DANA*
+┣⊱ _+6285772668375_
+┣━⊱ *PULSA*
+┣⊱ _+6285772668375_
+┣━⊱ *GOPAY*
+┣⊱ _+6285772668375_
+┣━⊱ *OVO*
+┣⊱ _085772668375_
+┣━⊱ *SAWERIA YANG PUNYA SCRIPT*
+┣⊱ _https://saweria.co/ampibibot_
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
